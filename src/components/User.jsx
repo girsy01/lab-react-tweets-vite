bf0d67c239b965a203a2e7d20163dfbafcc49c88
Franziska Scheer <franzi-scheer@web.de>
2024-10-26 17:57:47 +0200

    lab done

---
+++ src/components/User.jsx
@@ -0,0 +1,8 @@
+export const User = ({ user }) => {
+  return (
+    <span className="user">
+      <span className="name">{user.name}</span>
+      <span className="handle">{user.handle}</span>
+    </span>
+  );
+};
